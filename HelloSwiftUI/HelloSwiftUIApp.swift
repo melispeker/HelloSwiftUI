bcdd1ad175f768e626c1bfe726ddbd9b54b10b76
Melis Peker <melis.peker@hisarschool.k12.tr>
2024-12-15 22:27:11 +0300

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Melis Peker on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
